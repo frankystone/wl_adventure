6c5e0ce4e80da9da30767922b3740cd828e867f2
kaputtnik <somal@arcor.de>
2020-08-23 11:06:05 +0200

    added tribes folder

---
+++ The_Adventure.wmf/scripting/tribes/init.lua
@@ -0,0 +1,7 @@
+return {
+   atlanteans = {
+      buildings = {
+         "hq_poseidon",
+      }
+   }
+}
---
+++ The_Adventure.wmf/scripting/tribes/poseidon_hq/init.lua
@@ -0,0 +1,28 @@
+tribes = wl.Tribes()
+--dirname = "map:scripting/tribes/poseidon_hq/"
+--dirname = "tribes/buildings/trainingsites/atlanteans/labyrinth/"
+local dirname = path.dirname(__file__)
+print("Dirname", dirname)
+
+tribes:new_warehouse_type {
+   msgctxt = "atlanteans_building",
+   name = "hq_poseidon",
+   -- TRANSLATORS: This is a building name used in lists of buildings
+   descname = pgettext("atlanteans_building", "Headquarters"),
+   helptext_script = dirname .. "helptexts.lua",
+   icon = dirname .. "menu.png",
+   size = "big",
+   destructible = false,
+   --animation_directory = ""
+   animations = {
+      idle = {
+         pictures = path.list_files(dirname .. "idle_??.png"),
+              hotspot = { 80, 88 },
+      },
+   },
+
+   aihints = {},
+
+   heal_per_second = 220,
+   conquers = 2,
+}
